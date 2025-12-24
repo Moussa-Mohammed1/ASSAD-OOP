9867224762d13eaaeffa37b0ecf7c5186eed88d8
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-24 15:41:12 +0100

    revert: Update config.php file to and implement the database Class

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,20 @@
 <?php
-    include 'dbconnect.php';
-    $pdo = new DBconnection('localhost','assad','root','');
-    $pdo->DBconnect();
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'assad');
+
+    include 'database.php';
+    $pdo = new Database();
+    $value = 'Moussa';
+    $sql = 'SELECT * FROM utilisateur WHERE nom = :nom';
+    $pdo->query($sql);
+    $pdo->bind(':nom', $value);
+    $result = $pdo->get();
+    
+    foreach($result as $row){
+        echo $row->email;
+    }
+
+    
     
