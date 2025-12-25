af704326c4a0c062495cda6ff3e176dadbb293c5
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-25 21:59:39 +0100

    feat: Add EtapesVisites class attributes and methods

---
+++ Classes/etapesVisite.php
@@ -0,0 +1,31 @@
+<?php
+include './../Config/config.php';
+class EtapesVisites
+{
+    private $id_etape;
+    private $titreEtape;
+    private $descriptionEtape;
+    private $ordreEtape;
+    private $id_visite;
+
+    public function  __construct($titreEtape, $descriptionEtape, $ordreEtape, $id_visite)
+    {
+        $this->titreEtape = $titreEtape;
+        $this->descriptionEtape = $descriptionEtape;
+        $this->ordreEtape = $ordreEtape;
+        $this->id_visite = $id_visite;
+    }
+
+    public function addEtape()
+    {
+        $pdo = new Database();
+        $sql = 'INSERT INTO etapesvisite(titreetape ,descriptionetape ,ordreetape ,id_visite)
+                VALUES (:titre, :descr, :ordre, :idv ';
+        $pdo->query($sql);
+        $pdo->bind(':titre', $this->titreEtape);
+        $pdo->bind(':descr', $this->descriptionEtape);
+        $pdo->bind(':ordre', $this->ordreEtape);
+        $pdo->bind(':idv', $this->id_visite);
+        $pdo->execute();
+    }
+}
